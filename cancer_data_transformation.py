0b4cdf76fb61ab71b44404408ebcea5666b5ad4d
Berkay-Bozkurt <berkay.bb.bozkurt@gmail.com>
2023-06-12 11:17:27 +0200

    first push

--- cancer_data_transformation.py
+++ cancer_data_transformation.py
@@ -12,4 +12,17 @@ def move_images(data, source_folder, destination_parent_folder):
         destination_folder = os.path.join(destination_parent_folder, diagnosis)
         os.makedirs(destination_folder, exist_ok=True)
 
+    # Move files to the respective destination folders
+    for index, row in data.iterrows():
+        img_id = row['img_id']
+        diagnostic = row['diagnostic']
+        file_name = img_id
+        source_path = os.path.join(source_folder, file_name)
+        destination_folder = os.path.join(destination_parent_folder, diagnostic)
+        destination_path = os.path.join(destination_folder, file_name)
 
+        if os.path.exists(source_path):
+            shutil.move(source_path, destination_path)
+            print(f"Moved {file_name} to {destination_folder}/{file_name}")
+        else:
+            print(f"File {file_name} not found in {source_folder}")
