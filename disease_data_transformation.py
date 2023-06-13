f60abe33d8112602eb66901bd4b636ca7ddf653f
Berkay-Bozkurt <berkay.bb.bozkurt@gmail.com>
2023-06-13 10:41:30 +0200

    disease data transformation

--- disease_data_transformation.py
+++ disease_data_transformation.py
@@ -3,4 +3,15 @@ import requests
 import os
 csv_=pd.read_csv("fitzpatrick17k.csv")
 
+disease_list = [
+    "psoriasis",
+    "allergic contact dermatitis",
+    ["lupus erythematosus", "lupus subacute"],
+    "folliculitis",
+    "scabies",
+    "photodermatoses",
+    ["acne vulgaris", "acne"],
+    ["eczema", "dyshidrotic eczema"],
+    "seborrheic dermatitis"]
+
 
