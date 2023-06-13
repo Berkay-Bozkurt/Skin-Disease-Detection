a16d61b48065f6c2cb56dc64006bd9976db4357f
Berkay-Bozkurt <berkay.bb.bozkurt@gmail.com>
2023-06-13 10:41:56 +0200

    disease data transformation

--- disease_data_transformation.py
+++ disease_data_transformation.py
@@ -14,4 +14,18 @@ disease_list = [
     ["eczema", "dyshidrotic eczema"],
     "seborrheic dermatitis"]
 
+def cln_data(data, data_list):
+    for i in data_list:
+        if isinstance(i, list):
+            df = data[data["label"].isin(i)]
+            disease_name = '_'.join(i)
+        else:
+            df = data[data["label"] == i]
+            disease_name = i
 
+        df.reset_index(inplace=True)
+        df["pic_name"] = df["md5hash"]
+        data_dict = dict(zip(df["pic_name"], df["url"]))
+
+        folder_path = f"./{disease_name.replace(' ', '_', )}"
+        os.makedirs(folder_path, exist_ok=True)  # Create folder if 
